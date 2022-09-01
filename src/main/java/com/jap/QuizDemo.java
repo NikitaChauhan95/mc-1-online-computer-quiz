07ef164829bf826031eb053c8b1865a3e602d648
Nikita Chauhan <nikitachauhan64@gmail.com>
2022-09-01 16:37:27 +0530

    passed the test cases for highest score

--- src/main/java/com/jap/QuizDemo.java
+++ src/main/java/com/jap/QuizDemo.java
@@ -4,16 +4,38 @@ public class QuizDemo {
 
     //return the school name which has got the highest score in quiz
     //Handle the NumberFormatException
-    public String highestScore(String []nameOfSchool , String[] scores ){
+    public String highestScore(String[] nameOfSchool, String[] scores) {
+        int highestScore = 0;
+        String nameOfSchoolHavingHighestMarks = null;
+        try {
+            int[] scoresInt = new int[scores.length];
+            for (int index = 0; index < scores.length; index++) {
+                scoresInt[index] = Integer.parseInt(scores[index]);
+            }
+            for (int index = 0; index < scores.length; index++) {
+                if (highestScore < scoresInt[index]) ;
+                highestScore = scoresInt[index];
 
 
-        return "";
+            }
 
+            return "nameOfSchoolHavingHighestMarks";
+        } catch (NumberFormatException exception) {
+            return exception.toString();
+        }
     }
+
+
     //convert all the names in uppercase
     //Handle the NullPointerException
-    public String[] convertAllNamesToCapital(String name[]){
-        String upperCase[] = new String[0];
+    public String[] convertAllNamesToCapital(String[] name) {
+        String[] upperCase = new String[name.length];
+        try
+        {
+            for (int counter = 0; counter < name.length; counter++) {
+                upperCase[inde]
+            }
+        }
 
         return upperCase;
     }
